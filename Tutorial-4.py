fa43c511e0bd0f9935d6ae64d18e833060ce8cd7
Tejas Talekar <tjstlekr@gmail.com>
2016-09-04 18:36:48 +0530

    Introduction to grid(tabular format) alligning using sticky

---
+++ Tutorial-4.py
@@ -0,0 +1,20 @@
+from Tkinter import *
+
+root = Tk()
+#grid configures the widget in a tabular format
+
+
+label = Label(root,text="Name")
+label2 = Label(root,text="Password")
+entry_1 =Entry(root)    #textbox
+entry_2 = Entry(root)
+
+label.grid(row=0,sticky=E)
+label2.grid(row=1,sticky=E)         #sticky is used for alligning the labels in East direction :D
+                                    #perform the program without using sticky argument
+
+entry_1.grid(row=0,column=1)
+entry_2.grid(row=1,column=1)        #row and column in grid
+
+
+root.mainloop()
